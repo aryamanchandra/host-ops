e52ce471953b283f4690a49cda1654e37ca6918a
aryamanchandra <aryamanhere@gmail.com>
2025-11-20 13:54:01 +0530

    infra: add geoip-lite dep and externalize its data files in next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    // geoip-lite ships binary .dat data files that must not be bundled by
+    // webpack; resolve them from node_modules at runtime instead.
+    serverComponentsExternalPackages: ['geoip-lite'],
+  },
   images: {
     remotePatterns: [
       {
